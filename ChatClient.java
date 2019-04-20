8eb819b9b4b94071491a3f29a5ece4e43bcfb961
Michael Vasile <michaelvasile17@gmail.com>
2019-04-19 20:08:32 -0400

    Convert ChatClient to JPanel

--- ChatClient.java
+++ ChatClient.java
@@ -16,7 +16,7 @@ import java.text.SimpleDateFormat;
  * @author Lucas Kohorst
  * @version 3/25/19
  */
-public class ChatClient extends JFrame {
+public class ChatClient extends JPanel {
 
    // For RMI
    private GameStub stub;
@@ -39,7 +39,7 @@ public class ChatClient extends JFrame {
     * @param name the name of the current game
     */
    public ChatClient(String ip, String stubID, String name) { 
-      super(name);
+
       makeGUI();
 
       this.name = name;
@@ -63,33 +63,19 @@ public class ChatClient extends JFrame {
     */
    public void makeGUI() {
 
-      String[] optionsName = {"OK"};
-      JPanel panelName = new JPanel();
-      JLabel lblName = new JLabel("Enter your nickname: ");
-      JTextField txtName = new JTextField(10);
-      panelName.add(lblName);
-      panelName.add(txtName);
-      JOptionPane.showOptionDialog(null, panelName, "Nickname", 
-             JOptionPane.NO_OPTION, JOptionPane.QUESTION_MESSAGE,
-            null, optionsName, optionsName[0]);
-      nickname = txtName.getText();
-
-      // Getting the username
-      if (nickname.length() < 1) {
-         nickname = "Anonymous";
-      }
+      setLayout(new BorderLayout(5,10));
 
       // JTextArea for Chat
-      chatArea = new JTextArea(20, 50);
+      chatArea = new JTextArea(30,20);
       chatArea.setLineWrap(true);
       chatArea.setEditable(false);
 
       // Adding the chat area to the frame
-      add(chatArea, "Center");
+      add(chatArea, BorderLayout.NORTH);
 
       // JScrollPane for ChatArea
       JScrollPane jsp = new JScrollPane(chatArea);
-      add(jsp, "Center");
+      add(jsp, BorderLayout.NORTH);
 
       // JPanel for input field and action button
       JPanel inputArea = new JPanel();
@@ -111,42 +97,7 @@ public class ChatClient extends JFrame {
       });
 
       // Adding to the frame
-      add(inputArea, "South");
-
-      // Send exit message
-      addWindowListener(new WindowAdapter() {
-         @Override
-         public void windowClosing(WindowEvent e) {
-            sendMessage(nickname + " has left the chat");
-            System.out.println("Starting shutdown");
-            // Get the game that the client is running
-            // create registry and bind to the main server
-            // Creating the registry
-            try {
-               // Locating the Registry
-               Registry registry = LocateRegistry.getRegistry(ip);
-
-               // Looking up the ServerStub class
-               ServerStub serverStub = (ServerStub) registry.lookup(
-                      "ServerStub");
-
-               System.out.println("Connected to the server");
-
-               // Calling the shutdown method on the server
-               System.out.println(serverStub.shutdownClient(name));
-
-               // Getting rid of the client
-               dispose();
-
-               // Creating the Lobby Again
-               new Lobby(ip);
-
-            } catch (Exception oe) {
-               System.err.println("Client exception: " + oe.toString());
-               oe.printStackTrace();
-            }
-         }
-      });
+      add(inputArea, BorderLayout.CENTER);
 
       // Adding the keylistener
       inputField.addKeyListener(new KeyAdapter() {
@@ -169,12 +120,6 @@ public class ChatClient extends JFrame {
       // Creating the timer to continually update the GUI
       Timer time = new Timer();
       time.schedule(new MessageTimer(), 500, 1500);
-
-      // Set JFrame sizing
-      setSize(350, 650);
-      setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
-      setVisible(true);
-      setLocation(1500, 300);
    } 
 
    /**
